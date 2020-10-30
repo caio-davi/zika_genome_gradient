e95369ad8b9ebc6fdd3573d7e77ea2703d385a73
Caio Davi <caio.davi@tamu.edu>
2020-10-30 17:44:30 -0500

    Changing to relative paths

--- metrics/holdout_validation.py
+++ metrics/holdout_validation.py
@@ -5,13 +5,22 @@ Created on Tue Jan  7 13:51:01 2020
 
 @author: flavio
 """
+from sklearn.metrics import classification_report
+from sklearn.metrics import f1_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+from sklearn import tree
+import sklearn.neighbors
+from sklearn.metrics import accuracy_score
+from sklearn.metrics.classification import confusion_matrix
 import numpy as np
 from plot_roc_curve import plot_roc_curve
 
-x_train = np.load("x_train.npy")
-x_test = np.load("x_test.npy")
-y_train = np.load("y_train.npy")
-y_test = np.load("y_test.npy")
+x_train = np.load("../data_splits/x_train.npy")
+x_test = np.load("../data_splits/x_test.npy")
+y_train = np.load("../data_splits/y_train.npy")
+y_test = np.load("../data_splits/y_test.npy")
 
 s = []
 for i in y_train:
@@ -30,32 +39,21 @@ for i in y_test:
 y_test = s
 
 
-from sklearn.metrics.classification import confusion_matrix
-from sklearn.metrics import accuracy_score
-import sklearn.neighbors
-from sklearn import tree
-from sklearn import svm
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import f1_score
-from sklearn.metrics import classification_report
-    
-model0 = [LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial'),"Logistic Regression"]
-model1 = [sklearn.neighbors.KNeighborsClassifier(n_neighbors=1),'KNN']
-model2 = [tree.DecisionTreeClassifier(),'Decision Tree']
-model3 = [svm.SVC(kernel='rbf',probability=True),'SVM-RBF Kernel']
-model4 = [svm.SVC(kernel='linear',probability=True),'SVM-Linear Kernel']
-models = [model0,model1,model2,model3,model4]
-#models = [model4]
+model0 = [LogisticRegression(
+    random_state=0, solver='lbfgs', multi_class='multinomial'), "Logistic Regression"]
+model1 = [sklearn.neighbors.KNeighborsClassifier(n_neighbors=1), 'KNN']
+model2 = [tree.DecisionTreeClassifier(), 'Decision Tree']
+model3 = [svm.SVC(kernel='rbf', probability=True), 'SVM-RBF Kernel']
+model4 = [svm.SVC(kernel='linear', probability=True), 'SVM-Linear Kernel']
+models = [model0, model1, model2, model3, model4]
 
 for m in models:
     model = m[0]
-    model.fit(x_train,y_train)
+    model.fit(x_train, y_train)
     y_predicted = model.predict(x_test)
-    accuracy = accuracy_score(y_test,y_predicted)
+    accuracy = accuracy_score(y_test, y_predicted)
     print(m[1])
-    print("Accuracy = ",accuracy)
-    print(confusion_matrix(y_test,y_predicted))
-    #print(classification_report(y_test, y_predicted, target_names=["Afr","Amr"]))
-    
-    #plot_roc_curve(m[1],model,x_test,y_test)
+    print("Accuracy = ", accuracy)
+    print(confusion_matrix(y_test, y_predicted))
+    # print(classification_report(y_test, y_predicted, target_names=["Afr","Amr"]))
+    # plot_roc_curve(m[1],model,x_test,y_test)
