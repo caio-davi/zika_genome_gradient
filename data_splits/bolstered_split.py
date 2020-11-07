0d9683c07b13612a43d6a1a435832f1fa3cbecc1
Caio Davi <caio.davi@tamu.edu>
2020-11-07 11:15:29 -0600

    New sigma definition

--- data_splits/bolstered_split.py
+++ data_splits/bolstered_split.py
@@ -1,14 +1,26 @@
-from sklearn.model_selection import StratifiedShuffleSplit
-from sklearn.preprocessing import LabelEncoder
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from scipy.spatial.distance import cdist
+from scipy.stats import chi
 import random
+
+from sys import exit
 
 BOLSTER_MULTIPLIER = 100
 
+df = pd.read_csv("../dados_preprocessados.csv", header=0)
 
-def getSigmas(df):
-    return df.apply(lambda col: col.var())
+features = ["rs12610506", "rs1335531", "rs15895", "rs2629396", "rs3911403",
+            "rs41265961", "rs4143815", "rs4804801", "rs4963271", "rs7127254", "Class"]
+
+
+def getSigma(df):
+    D = cdist(df, df, 'euclid')
+    np.fill_diagonal(D, 99999999)
+    meanMinDist = np.mean(D.min(axis=1), axis=0)
+    cp = chi.ppf(0.5, df.shape[1])
+    return meanMinDist/cp
 
 
 def encodeDF(df):
@@ -16,23 +28,18 @@ def encodeDF(df):
     return df.apply(lambda col: LabelEncoder().fit_transform(col))
 
 
-def bolster(df, sigmas, new_points):
+def bolster(df, sigma, new_points):
     bolstered = pd.DataFrame()
     for sample in df.index:
         for m in range(new_points):
             bolsteredRow = pd.Series()
             for feature in df.columns:
                 new_point = random.gauss(
-                    mu=df.loc[sample, feature], sigma=sigmas[feature])
+                    mu=df.loc[sample, feature], sigma=sigma)
                 bolsteredRow[feature] = new_point
             bolstered = bolstered.append(bolsteredRow, ignore_index=True)
     return bolstered
 
-
-df = pd.read_csv("../dados_preprocessados.csv", header=0)
-
-features = ["rs12610506", "rs1335531", "rs15895", "rs2629396", "rs3911403",
-            "rs41265961", "rs4143815", "rs4804801", "rs4963271", "rs7127254", "Class"]
 
 df = df[features]
 
@@ -42,8 +49,8 @@ X_1 = encodeDF(df[df['Class'] == 2])                # Split and Encode Classes
 X_0 = X_0.drop(['Class'], axis=1)                   # Remove label column
 X_1 = X_1.drop(['Class'], axis=1)                   # Remove label column
 
-X_0_sig = getSigmas(X_0)                            # Generate features' sigmas
-X_1_sig = getSigmas(X_1)                            # Generate features' sigmas
+X_0_sig = getSigma(X_0)                            # Generate features' sigmas
+X_1_sig = getSigma(X_1)                            # Generate features' sigmas
 
 X = pd.concat([bolster(X_0, X_0_sig, BOLSTER_MULTIPLIER),
                bolster(X_1, X_1_sig, BOLSTER_MULTIPLIER)])
